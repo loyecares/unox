ca113e6cea3e19bbafb8e7ba0d46f6a34bd4ca55
eadelekeife <eadelekeife@gmail.com>
2023-04-02 09:02:57 +0100

    menu popup

--- js/common.js
+++ js/common.js
@@ -14,3 +14,19 @@ const closeMobileMenu = () => {
     //   duration: 0
     // })
 }
+
+document.querySelector('.nav-products').addEventListener('mouseover', () => {
+    document.querySelector('.hover-tab.products-tab').style.display = "block";
+})
+
+document.querySelector('.nav-more').addEventListener('mouseover', () => {
+    document.querySelector('.hover-tab.more-tab').style.display = "block"
+})
+
+document.querySelector('.nav-products').addEventListener('mouseout', () => {
+    document.querySelector('.hover-tab.products-tab').style.display = "none";
+})
+
+document.querySelector('.nav-more').addEventListener('mouseout', () => {
+    document.querySelector('.hover-tab.more-tab').style.display = "none"
+})
