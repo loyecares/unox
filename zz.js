a03786291d40674ab1dc6f83b00a18465e95a5c2
Adeleke Ifeoluwase <eadelekeife@gmail.com>
2023-05-02 11:48:02 -0700

    myunox element slide in

--- zz.js
+++ zz.js
@@ -1,53 +1,24 @@
 const tl = gsap.timeline({
     scrollTrigger: {
-      trigger: "#pinned-anim",
-      pin: "#pinned-anim",
-      start: "top top",
-      end: () => `+=100%`,
-      pin: true,
-      pinSpacing: true,
-      scrub: 1,
-      invalidateOnRefresh: true,
-      markers: true
+        trigger: ".pin-me-down",
+        pin: ".pin-me-down",
+        start: "top top",
+        end: () => `+=100%`,
+        pin: true,
+        pinSpacing: true,
+        scrub: 1,
+        invalidateOnRefresh: true,
+        markers: true
     },
-  });
-  
-  function pinStats() {
-    document.querySelector('.articles').style.visibility = "visible"; 
-    tl.fromTo(
-          ".slide-1",
-          { 
-              y: 0
-          },
-          {
-              y: '-100vh',
-          }, 0.5
-      );
-      tl.fromTo(
-          ".slide-2",
-          {
-              y: '100vh',
-          },
-          {
-              y: 0,
-          }, "<"
-      );
-      tl.to(".slide-2", {
-          y:  '-100vh',
-      }, "+=0.5");
-      tl.fromTo(
-          ".slide-3",
-          {
-              y: '100vh',
-          },
-          {
-              y: 0,
-          }, "<"
-      );
-    // just to add some extra time at the end so the last slide stays put for a bit before becoming unpinned
-    tl.to({}, {duration: 0.5});
-  }
-  
-  window.addEventListener("load", function (event) {
-      pinStats();
-  });
+});
+
+function pinStats() {
+    // document.querySelector('.articles').style.visibility = "visible";
+    tl.to(".box", {
+        y: 300
+    })
+}
+
+window.addEventListener("load", function (event) {
+    pinStats();
+});
